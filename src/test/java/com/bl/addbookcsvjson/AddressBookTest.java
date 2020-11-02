70f131e228a35eec97151590015799a11fba6074
Jagriti <jagriti.agni@gmail.com>
2020-11-03 03:29:44 +0530

    [Jagriti]ADDED: Test case to update email of a given contact

--- src/test/java/com/bl/addbookcsvjson/AddressBookTest.java
+++ src/test/java/com/bl/addbookcsvjson/AddressBookTest.java
@@ -1,7 +1,5 @@
 package com.bl.addbookcsvjson;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,14 +13,23 @@ public class AddressBookTest {
 	}
 	
 	@Test
-	public void givenAddressBookInDB_whenRetrieved_ShouldMatchBooksCount() {
+	public void givenAddressBookInDB_whenRetrieved_shouldMatchBooksCount() {
 		long countEntries = addressBookService.getAddressBookDataFromDB();
 		Assert.assertEquals(1, countEntries);
 	}
 	
 	@Test
-	public void givenAddressBookInDB_whenContactsRetrieved_ShouldMatchContactsCount() {
+	public void givenAddressBookInDB_whenContactsRetrieved_shouldMatchContactsCount() {
 		long countEntries = addressBookService.getContactsDataFromDB();
 		Assert.assertEquals(7, countEntries);
+	}
+	
+	// Update contact info
+	@Test
+	public void givenAddressBookInDB_whenContactsUpdated_shouldBeInSyncWithDB() {
+		addressBookService.getContactsDataFromDB();
+		addressBookService.updateContactEmail("Jag", "qwerty@1223.com");
+		boolean result = addressBookService.checkAddressBookContactsInSyncWithDB("Jag");
+		Assert.assertTrue(result);
 	}
 }
