27e12bac9dbdd31b73fbc0659a088402a2237a5e
Jagriti <jagriti.agni@gmail.com>
2020-11-03 20:48:40 +0530

    [Jagriti]ADDED: Test case to check contacts found by city name

--- src/test/java/com/bl/addbookcsvjson/AddressBookTest.java
+++ src/test/java/com/bl/addbookcsvjson/AddressBookTest.java
@@ -8,24 +8,24 @@ import org.junit.Test;
 
 public class AddressBookTest {
 	AddressBookManage addressBookService = null;
-	
+
 	@Before
 	public void initializeVariables() {
 		addressBookService = new AddressBookManage();
 	}
-	
+
 	@Test
 	public void givenAddressBookInDB_whenRetrieved_shouldMatchBooksCount() {
 		long countEntries = addressBookService.getAddressBookDataFromDB();
 		Assert.assertEquals(1, countEntries);
 	}
-	
+
 	@Test
 	public void givenAddressBookInDB_whenContactsRetrieved_shouldMatchContactsCount() {
 		long countEntries = addressBookService.getContactsDataFromDB();
 		Assert.assertEquals(7, countEntries);
 	}
-	
+
 	// Update contact info
 	@Test
 	public void givenAddressBookInDB_whenContactsUpdated_shouldBeInSyncWithDB() {
@@ -34,14 +34,23 @@ public class AddressBookTest {
 		boolean result = addressBookService.checkAddressBookContactsInSyncWithDB("Jag");
 		Assert.assertTrue(result);
 	}
-	
-	//contacts in given date range
+
+	// contacts in given date range
 	@Test
 	public void givenDataRange_shouldReturnContactsFromDBAddedInDateRange() {
 		addressBookService.getContactsDataFromDB();
 		String start = "2019-01-01";
 		String end = "2020-01-05";
 		List<ContactDetails> contactsDataList = addressBookService.getContactsByDateRange(start, end);
+		Assert.assertEquals(2, contactsDataList.size());
+	}
+
+	// contacts by city
+	@Test
+	public void givenAddressBookData_whenFindContactsByCity_shouldMatchContactsCount() {
+		addressBookService.getContactsDataFromDB();
+		String city = "Kota";
+		List<ContactDetails> contactsDataList = addressBookService.getContactsByCity(city);
 		Assert.assertEquals(2, contactsDataList.size());
 	}
 }
