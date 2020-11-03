2a9579a16f8e4d25288698e5972747eedee50f54
Jagriti <jagriti.agni@gmail.com>
2020-11-03 21:45:31 +0530

    [Jagriti]ADDED: Test case to check if the data added to all tables in db is in sync

--- src/test/java/com/bl/addbookcsvjson/AddressBookTest.java
+++ src/test/java/com/bl/addbookcsvjson/AddressBookTest.java
@@ -1,5 +1,6 @@
 package com.bl.addbookcsvjson;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.junit.Assert;
@@ -17,20 +18,20 @@ public class AddressBookTest {
 	@Test
 	public void givenAddressBookInDB_whenRetrieved_shouldMatchBooksCount() {
 		long countEntries = addressBookService.getAddressBookDataFromDB();
-		Assert.assertEquals(1, countEntries);
+		Assert.assertEquals(6, countEntries);
 	}
 
 	@Test
 	public void givenAddressBookInDB_whenContactsRetrieved_shouldMatchContactsCount() {
 		long countEntries = addressBookService.getContactsDataFromDB();
-		Assert.assertEquals(7, countEntries);
+		Assert.assertEquals(13, countEntries);
 	}
 
 	// Update contact info
 	@Test
 	public void givenAddressBookInDB_whenContactsUpdated_shouldBeInSyncWithDB() {
 		addressBookService.getContactsDataFromDB();
-		addressBookService.updateContactEmail("Jag", "qwerty@1223.com");
+		addressBookService.updateContactEmailInDB("Jag", "qwerty@1223.com");
 		boolean result = addressBookService.checkAddressBookContactsInSyncWithDB("Jag");
 		Assert.assertTrue(result);
 	}
@@ -41,8 +42,8 @@ public class AddressBookTest {
 		addressBookService.getContactsDataFromDB();
 		String start = "2019-01-01";
 		String end = "2020-01-05";
-		List<ContactDetails> contactsDataList = addressBookService.getContactsByDateRange(start, end);
-		Assert.assertEquals(2, contactsDataList.size());
+		List<ContactDetails> contactsDataList = addressBookService.getContactsByDateRangeFromDB(start, end);
+		Assert.assertEquals(4, contactsDataList.size());
 	}
 
 	// contacts by city
@@ -50,16 +51,29 @@ public class AddressBookTest {
 	public void givenAddressBookData_whenFindContactsByCity_shouldMatchContactsCount() {
 		addressBookService.getContactsDataFromDB();
 		String city = "Kota";
-		List<ContactDetails> contactsDataList = addressBookService.getContactsByCity(city);
+		List<ContactDetails> contactsDataList = addressBookService.getContactsByCityFromDB(city);
 		Assert.assertEquals(2, contactsDataList.size());
 	}
-	
+
 	// contacts by state
 	@Test
 	public void givenAddressBookData_whenFindContactsByState_shouldMatchContactsCount() {
 		addressBookService.getContactsDataFromDB();
 		String state = "Rajasthan";
-		List<ContactDetails> contactsDataList = addressBookService.getContactsByState(state);
+		List<ContactDetails> contactsDataList = addressBookService.getContactsByStateFromDB(state);
 		Assert.assertEquals(2, contactsDataList.size());
 	}
+
+	// add address book data into database
+	@Test
+	public void givenData_whenAdded_shouldBeInSyncWithDB() {
+		addressBookService.getContactsDataFromDB();
+		String date = "2017-02-05";
+		String contactType = "Office";
+		addressBookService.addContactsDetailsInDB("thirdBook", "Shaam","Mohan", "India" , "Mumbai",
+				"MH", "100222", "12365470085", "Shyam@123.com", date, contactType);
+		boolean result = addressBookService.checkAddressBookContactsInSyncWithDB("Shaam");
+		Assert.assertTrue(result);
+	}
+
 }
